857050b5830204488d35004db092af9cdd21ce93
Christopher Neal <christopher.m.neal@gmail.com>
2021-10-27 10:24:17 -0700

    Pass second test to allow for different capitalization
    Co-authored-by: Christy Welch <christinamawel@gmail.com>”

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function pigLatinTranslator(word) {
-  const vowelArray = ["a", "e", "i", "o", "u"];
+  const vowelArray = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
   let pigLatin = "";
   if (vowelArray.includes(word[0])) {
     pigLatin = (word + "-way");
