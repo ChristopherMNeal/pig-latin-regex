48d0088d64eda69880770cc34d709b85abd8658e
Christopher Neal <christopher.m.neal@gmail.com>
2021-10-27 14:10:55 -0700

    Add business logic to account for punctuation in text
    Co-authored-by: Christy Welch <christinamawel@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,53 @@
 function pigLatinTranslator(word) {
   const vowelArray = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
-  let pigLatinArray = [];
+  const punctuation = [".", "!", ",", '"', "'", "#", "(", ")", "?"]
+  let pigLatinString = "";
   const wordArray = word.split("");
-  console.log(wordArray);
-  if (vowelArray.includes(wordArray[0])) {
-    pigLatinArray = wordArray.slice(0, wordArray.length);
-    pigLatinArray.push("-way")
-    console.log(wordArray);
-  } else {
+  const modWordArray = wordArray.slice(0, wordArray.length);
+  let beforePunctuation = [];
+  let afterPunctuation = [];
   for (let i = 0; i < wordArray.length; i++) {
-    if (vowelArray.includes(wordArray[i])) {
-      if (wordArray[i] === "u" && wordArray[i-1] === "q") {
-        const consonants = wordArray.slice(0, i+1);
-        pigLatinArray = wordArray.slice(i+1, wordArray.length);
-        pigLatinArray.push("-", consonants.join(""), "ay")
-        console.log(pigLatinArray);
+     if (punctuation.includes(wordArray[i])) {
+      beforePunctuation.push(wordArray[i])
+      modWordArray.shift();
+     } else {
+       beforePunctuation = beforePunctuation.join("")
+       break;
+     }
+  }
+  for (let i = wordArray.length-1; i >= 0; i--) {
+      if (punctuation.includes(wordArray[i])) {
+        afterPunctuation.push(wordArray[i])
+        modWordArray.pop();
+    } else {
+        afterPunctuation = afterPunctuation.join("")
         break;
-      }
-      const consonants = wordArray.slice(0, i);
-      pigLatinArray = wordArray.slice(i, wordArray.length);
-      pigLatinArray.push("-", consonants.join(""), "ay")
-      console.log(pigLatinArray);
-      break;
     }
   }
-  return pigLatinArray.join("");
+
+  console.log(beforePunctuation);
+  console.log(afterPunctuation);
+  console.log(modWordArray);
+  
+  if (vowelArray.includes(modWordArray[0])) {
+    pigLatinString = modWordArray.slice(0, modWordArray.length).join("") + "-way";
+    console.log(modWordArray);
+  } else {
+  for (let i = 0; i < modWordArray.length; i++) {
+    if (vowelArray.includes(modWordArray[i])) {
+      if (modWordArray[i] === "u" && modWordArray[i-1] === "q") {
+        const consonants = modWordArray.slice(0, i+1);
+        pigLatinString = beforePunctuation + modWordArray.slice(i+1, modWordArray.length).join("") + "-" + consonants.join("") + "ay" + afterPunctuation;
+        break;
+        } else {
+        const consonants = modWordArray.slice(0, i);
+        pigLatinString = beforePunctuation + modWordArray.slice(i, modWordArray.length).join("") + "-" + consonants.join("") + "ay" + afterPunctuation;
+        break;
+        }
+      }
+    }
   }
+  return pigLatinString;
 }
+
+console.log(pigLatinTranslator("##squab##"));
